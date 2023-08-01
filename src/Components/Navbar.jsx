75b418728d065571619d9f299954393e227edaf3
abhishek96k <abhisheknalawade2711@gmail.com>
2023-08-01 14:12:22 +0530

    initial

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -2,11 +2,13 @@ import React from 'react'
 
 function Navbar () {
   return (
+    <a href='/'>
     <div className="bg-black sticky top-0 bg-fixed text-3xl flex justify-between text-blue-500 font-bold p-3 border-b-2 border-gray-500">
       <span>
         SpaceX<span className="text-white">Blog</span>
       </span>
     </div>
+    </a>
   )
 }
 
